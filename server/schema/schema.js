94ed796b11380569e5678b6ee7d15e5e28f573f0
CaramonH <caramonh@yahoo.com>
2024-03-06 13:08:00 -0600

    Task 0, server setup

---
+++ server/schema/schema.js
@@ -0,0 +1,16 @@
+// Set up Schema and require GraphQL
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString } = require('graphql');
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  description: 'This represents a Task',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  })
+});
+
+module.exports = { TaskType };
