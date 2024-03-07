a6736cfdc7f06d66e0cd5a91b562c5585a032263
CaramonH <caramonh@yahoo.com>
2024-03-07 11:16:25 -0600

    Task 2 - changed schema require call to correctly define schema

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
 const { TaskType } = require('./schema/schema');
 
 const app = express();
